44d76bf5b411e32c83bed587ebeeeeb9de5a1973
Joinner Medina <devjoi2018@gmail.com>
2025-03-19 00:11:18 -0600

    Implementación del archivo de exportación de excepciones
    --- Se ha creado el archivo `exceptions.dart`, que centraliza la exportación de todas las excepciones del sistema, incluyendo `BaseException`, `AppException`, `DataGeneratorException` y `JsonGeneratorException`. Esta implementación mejora la organización del código al facilitar la importación de excepciones y promueve un manejo de errores más estructurado y accesible en el sistema.

---
+++ lib/core/exceptions/exceptions.dart
@@ -0,0 +1,9 @@
+/// Exporta todas las excepciones del sistema para facilitar su importación
+
+// Base de excepciones
+export 'base_exception.dart';
+
+// Excepciones específicas
+export 'app_exception.dart';
+export 'data_generator_exception.dart';
+export 'json_generator_exception.dart';
