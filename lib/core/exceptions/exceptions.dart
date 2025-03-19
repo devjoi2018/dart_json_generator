faf9be6a095adca1b97bd63f2df19179c632e094
Joinner Medina <devjoi2018@gmail.com>
2025-03-19 13:51:37 -0600

    Se añade la exportación de `CompressionException` en el módulo de excepciones. Esta inclusión permite un manejo más específico de errores relacionados con la compresión en la aplicación, facilitando la gestión de excepciones en contextos donde se utilicen compresores de datos.

--- lib/core/exceptions/exceptions.dart
+++ lib/core/exceptions/exceptions.dart
@@ -9,3 +9,4 @@ export 'data_generator_exception.dart';
 export 'json_generator_exception.dart';
 export 'file_exception.dart';
 export 'schema_validation_exception.dart';
+export 'compression_exception.dart';
