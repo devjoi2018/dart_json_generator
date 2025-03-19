7a618d0eb02a3919007ef15526e26846ee6a788e
Joinner Medina <devjoi2018@gmail.com>
2025-03-19 13:29:12 -0600

    Actualización de la interfaz `JsonGeneratorInterface` para mejorar la generación de JSON
    --- Se ha modificado la interfaz `JsonGeneratorInterface` para incluir un nuevo parámetro opcional `format` en los métodos `generateJson` y `generateJsonList`. Además, se ha mejorado la documentación de los métodos, especificando claramente su funcionalidad y los tipos de datos que manejan. Estas mejoras aumentan la flexibilidad y claridad en la generación de datos JSON.

--- lib/core/interfaces/json_generator_interface.dart
+++ lib/core/interfaces/json_generator_interface.dart
@@ -1,13 +1,19 @@
-/// Interfaz que define el contrato para los generadores de JSON
+/// Interfaz para el generador de JSON
 abstract class JsonGeneratorInterface {
-  /// Genera un JSON con un solo objeto
-  void generateJson({required String jsonName, required Function(Map<String, dynamic> data) jsonMap, int? seed});
+  /// Genera un objeto JSON
+  void generateJson({
+    required String jsonName,
+    required Function(Map<String, dynamic> data) jsonMap,
+    int? seed,
+    String? format,
+  });
 
-  /// Genera un JSON con una lista de objetos
+  /// Genera una lista de objetos JSON
   void generateJsonList({
     required String jsonName,
     required Function(List<Map<String, dynamic>> data) jsonMap,
-    bool addIdAutoincrement,
+    bool addIdAutoincrement = false,
     int? recordCount,
+    String? format,
   });
 }
