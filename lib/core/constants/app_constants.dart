dbeb23c1e49023293e6ee3b33f03d5a00c2985c1
Joinner Medina <devjoi2018@gmail.com>
2025-03-19 00:09:48 -0600

    Implementación de constantes de aplicación
    --- Se ha creado el archivo `app_constants.dart` que define constantes utilizadas en toda la aplicación, incluyendo la ruta base para la salida de archivos JSON, la ruta base para archivos de configuración, la versión de la aplicación y el nombre de la aplicación. Esta adición mejora la organización y la mantenibilidad del código al centralizar la configuración de constantes en un solo lugar.

---
+++ lib/core/constants/app_constants.dart
@@ -0,0 +1,14 @@
+/// Constantes utilizadas en toda la aplicación
+class AppConstants {
+  /// Ruta base para la salida de archivos JSON
+  static const String outputBasePath = 'lib/output/';
+
+  /// Ruta base para los archivos de configuración
+  static const String configBasePath = 'config/';
+
+  /// Versión de la aplicación
+  static const String appVersion = '1.1.1';
+
+  /// Nombre de la aplicación
+  static const String appName = 'Dart JSON Generator';
+}
